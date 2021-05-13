ca346d714aee5975ce66d83f6f5e357d908d2247
mnjowe <emmanuelmnjowe@gmail.com>
2021-05-13 14:25:52 +0200

    the very first commit - redesigning the home page

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Sign in | Rubi's Production Management Software</title>
+	<meta name="description" content="A software intended to provide complete solutions to  manufacturers to manage their raw materials inventory, the production process and product sells"/>
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/custom-css.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<link rel="icon" type="image/png" href="assets/img/logo/rubi_icon.png"/>
+</head>
+<body>
+	<img class="wave" src="assets/img/wave.png">
+	<div class="container">
+		<div class="img">
+			<img src="assets/img/set.png">
+		</div>
+		<div class="login-content">
+			<form action="index.html">
+				<img src="assets/img/logo/logo.png" style="height:60px">
+				<div class="login-text">Log In</div>
+				<div class="input-div one">
+					<div class="i">
+						<i class="fas fa-user"></i>
+					</div>
+					<div class="div">
+						<h5>Username</h5>
+						<input type="text" class="input">
+					</div>
+				</div>
+				<div class="input-div pass">
+					<div class="i">
+						<i class="fas fa-lock"></i>
+					</div>
+					<div class="div">
+						<h5>Password</h5>
+						<input type="password" class="input">
+					</div>
+				</div>
+				<a href="#">Forgot Password?</a>
+				<input type="submit" class="btn" value="Login">
+			</form>
+		</div>
+	</div>
+	<script type="text/javascript" src="assets/js/extra-js.js"></script>
+	<script type="text/javascript" src="assets/js/main.js"></script>
+</body>
+</html>
